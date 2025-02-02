efd028b121351c6e693d99ccecc9420e57fcc4c6
Andréa Nicole <andreanicolesm.22@gmail.com>
2025-02-01 23:28:32 -0300

    Site Update
    -Overhauled portfolio section
    -Changed javascript to more accurately set user language

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+// The locale our app first shows
+const defaultLocale = "en";
+
+const supportedLocales = ["en", "pt"];
+
+const fullyQualifiedLocaleDefaults = {
+  en: "en-US",
+  pt: "pt-BR",
+};
+
+// The active locale
+let locale;
+
+function isSupported(locale) {
+  return supportedLocales.indexOf(locale) > -1;
+}
+
+function supportedOrDefault(locales) {
+  return locales.find(isSupported) || defaultLocale;
+}
+
+function browserLocales(languageCodeOnly = false) {
+  return navigator.languages.map((locale) =>
+    languageCodeOnly ? locale.split("-")[0] : locale,
+  );}
+
+async function setLocale(newLocale) {
+  if (newLocale === locale) return;
+  
+  const langData = await fetchLanguageData(newLocale);
+  
+  locale = newLocale;
+  document.documentElement.lang = newLocale;
+
+  
+  updateContent(langData);
+}
+
 // Function to update content based on selected language
 function updateContent(langData) {
   document.querySelectorAll("[data-i18n]").forEach((element) => {
@@ -26,12 +64,32 @@ async function changeLanguage(lang) {
   updateContent(langData);
 }
 
-// Call updateContent() on page load
-window.addEventListener("DOMContentLoaded", async () => {
-  const userPreferredLanguage = localStorage.getItem("language") || "pt-BR";
-  const langData = await fetchLanguageData(userPreferredLanguage);
-  updateContent(langData);
+function bindLocaleSwitcher(initialValue) {
+  const switcher = document.querySelector(
+    "[data-i18n-switcher]",
+  );
+
+  switcher.value = initialValue;
+
+  switcher.onchange = (e) => {
+    setLocale(e.target.value);
+  };
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const initialLocale = supportedOrDefault(
+    browserLocales(true),
+  );
+
+  setLocale(initialLocale);
+  bindLocaleSwitcher(initialLocale);
 });
 
-// var userLang = navigator.language || navigator.userLanguage;
-// alert(userLang);
+
+// Call updateContent() on page load
+// window.addEventListener("DOMContentLoaded", async () => {
+//   const userPreferredLanguage = locale || "en";
+//   const langData = await fetchLanguageData(userPreferredLanguage);
+//   updateContent(langData);
+// });
+
